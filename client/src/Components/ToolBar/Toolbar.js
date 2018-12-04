2ec32e803f006742dd00df9732508432b13d3bd0
Allan Hartigan <allan.hartigan@gmail.com>
2018-12-03 20:18:52 -0500

    Testing react

---
+++ client/src/Components/ToolBar/Toolbar.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const toolbar = () => (
+<div>
+  <p>This is a test!</p>
+</div>
+);
+
+export default toolbar;
--- server.js
+++ server.js
@@ -1,17 +1,29 @@
 const express = require("express");
 const path = require("path");
-const mongoose = require("mongoose");
-const routes = require("./routes");
+//const mongoose = require("mongoose");
+//const routes = require("./routes");
+const axios = require("axios");
 const db = require("./models");
-
-const http = require('http').Server(app);
-const socket = require("socket.io");
+//const socket = require("socket.io");
 const app = express();
-const PORT = process.env.PORT || 3001;
+//const http = require('http').Server(app);
+const PORT = process.env.PORT || 8080;
+
+//const MONGODB_URI = process.env.MONGODB_URI || "http://localhost:8080"
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+//mongoose.connect(MONGODB_URI);
+
+// Make Public a static folder
+app.use(express.static(path.join(__dirname, 'client/public')));
+
+app.get('/', function(req, res) {
+  res.sendfile(path.resolve(__dirname, 'public', 'index.html'));
+});
+
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
@@ -21,12 +33,12 @@ if (process.env.NODE_ENV === "production") {
 
 
 // Define API routes here
-app.use(routes);
+//app.use(routes);
 
 // Send every other request to the React app
 // Define any API routes before this runs
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+  res.sendFile(path.join(__dirname, "./client/public/index.html"));
 });
 
 app.listen(PORT, () => {
