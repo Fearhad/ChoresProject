c339948928271115a6df1b2eb07b62c00b9347fc
Allan Hartigan <allan.hartigan@gmail.com>
2018-12-12 07:27:03 -0500

    Modified Launch page

--- client/src/pages/homepage.js
+++ client/src/pages/homepage.js
@@ -1,15 +1,33 @@
 import React from 'react';
 import { Link } from "react-router-dom";
+import classes from './homepage.css';
+import Wrapper from '../hoc/Wrapper/Wrapper'
+
 
 
 function HomePage () {
     return (
-        <div className="container">
-            <h1>This is the Chores Project</h1>
-            <p>Let's see how much allowance your children have earned!</p>            
-            <Link to={"/parent"}>I am a parent.</Link>
+        <Wrapper>
+            <div className={classes.Homepage}>
+        <div>
+       
+            <h1>Welcome to YourChores</h1>
+            <h2>Motivate your children to complete tasks by providing monetary and non-monetary rewards</h2>            
+            <Link to={"/parent"}><h6>Enter Site</h6></Link>
+            <article>
+  <p>Clean your room</p>
+  <p>Empty dishwasher</p>
+  <p>Mow the lawn</p>
+  <p>Walk the dog</p>
+  <p>Take out the garbage</p>
+  <p>Vacuum the carpet</p>
+  <p>Do the laundry</p>
+  <p>Shovel the driveway</p>
+</article>
             
         </div>
+        </div>
+        </Wrapper>
     );
 }
 
